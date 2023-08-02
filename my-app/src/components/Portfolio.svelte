188d380cd03cf04a42a7671553bcb763ee395e94
Rowan <volkwyn.rowan@gmail.com>
2023-08-02 16:00:43 +1000

    creating structure for portfolio page

---
+++ my-app/src/components/Portfolio.svelte
@@ -0,0 +1,79 @@
+<script>
+
+    import data from '../data.js';
+
+    let denomination = 'USD';
+    let filter = 'crypto';
+
+    function selectCurrency(Event) {
+        denomination = Event.target.value;
+    }
+
+    function filterAssets(event) {
+        filter = event.target.value;
+    }
+    
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <h1>Finances</h1>
+    <select id="currencyDropdown" on:change={selectCurrency}>
+        <option value="USD">US Dollar</option>
+        <option value="AUD">AU Dollar</option>
+        <option value="BTC">Bitcoin</option>
+    </select>
+    <select id="filterDropdown" on:change={filterAssets}>
+        <option value="all">All</option>
+        <option value="crypto">Crypto</option>
+        <option value="stocks">Stocks</option>
+        <option value="cash">Cash</option>
+    </select>
+    <div>
+        <table>
+            <table>
+                <thead>
+                  <tr>
+                    <th>Ticker</th>
+                    <th>Amount</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <!-- Display all crypto -->
+                <tbody>
+                  {#each data.crypto as item}
+                    <tr>
+                      <td>{item.ticker}</td>
+                      <td>{item.amount}</td>
+                      <td>{item.price}</td>
+                    </tr>
+                  {/each}
+                </tbody>
+                <!-- Display all stocks -->
+                <tbody>
+                    {#each data.stocks as item}
+                      <tr>
+                        <td>{item.ticker}</td>
+                        <td>{item.amount}</td>
+                        <td>{item.price}</td>
+                      </tr>
+                    {/each}
+                  </tbody>
+                  <!-- Display all cash -->
+                  <tbody>
+                    {#each data.cash as item}
+                      <tr>
+                        <td>{item.ticker}</td>
+                        <td>{item.amount}</td>
+                        <td>{item.price}</td>
+                      </tr>
+                    {/each}
+                  </tbody>
+              </table>
+        </table>
+
+    </div>
+</div>
---
+++ my-app/src/data.js
@@ -0,0 +1,40 @@
+const data = {
+    stocks: [
+        {
+            ticker: 'SPY',
+            amount: 100,
+            price: 456.78
+        },
+        {
+            ticker: 'TSLA',
+            amount: 40,
+            price: 231.07
+        }
+    ],
+    crypto: [
+        {
+            ticker: 'BTC',
+            amount: 0.8,
+            price: 29516.78
+        },
+        {
+            ticker: 'ETH',
+            amount: 5.6,
+            price: 1890.07
+        },
+        {
+            ticker: 'NEAR',
+            amount: 3000,
+            price: 1.41
+        }
+    ],
+    cash: [
+        {
+            ticker: 'AUD',
+            amount: 15000,
+            price: 0.6580   
+        }
+    ],
+};
+    
+export default data;
